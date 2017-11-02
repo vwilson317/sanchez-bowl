0a1a5b12aaf7e7e619b5cfaac87bc3a176d7dedd
Vincent Wilson <vwilson@cardlytics.com>
2017-11-02 17:42:03 -0400

    renaming solution adding call to api to get team

--- nfl.fantasy.sanchez.bowl.webapp/nfl.fantasy.sanchez.bowl.webapp/Controllers/HomeController.cs
+++ nfl.fantasy.sanchez.bowl.webapp/nfl.fantasy.sanchez.bowl.webapp/Controllers/HomeController.cs
@@ -11,32 +11,6 @@ namespace nfl.fantasy.sanchez.bowl.webapp.Controllers
 {
     public class HomeController : Controller
     {
-        public Team TeamOne { get; set; }
-        public Team TeamTwo { get; set; }
-
-        private readonly IPlayerDetailsHelper _playerDetailsHelper;
-        private readonly TeamConfig _teamConfigOptions;
-
-        public HomeController(IPlayerDetailsHelper playerDetailsHelper, IOptions<TeamConfig> teamConfigAccessor)
-        {
-            _playerDetailsHelper = playerDetailsHelper;
-            _teamConfigOptions = teamConfigAccessor.Value;
-        }
-
-        //public async Task OnGet()
-        //{
-        //    var queryStr = Request.QueryString.Value;
-        //    var queryParamObj = RequestQueryStrParser<QueryParamObj>.Parse(queryStr);
-        //    var week = queryParamObj.Week;
-        //    TeamOne = new Team(TeamIdentifier.Micheal);
-        //    var rosterOne = await _playerDetailsHelper.GetPlayerDetails(TeamOne.TeamIdentifierIdentifier, week);
-        //    TeamOne.Roster = rosterOne;
-
-        //    TeamTwo = new Team(TeamIdentifier.Dustin);
-        //    var rosterTwo = await _playerDetailsHelper.GetPlayerDetails(TeamTwo.TeamIdentifierIdentifier, week);
-        //    TeamTwo.Roster = rosterTwo;
-        //}
-
         public IActionResult Index()
         {
             return View();
--- nfl.fantasy.sanchez.bowl.webapp/nfl.fantasy.sanchez.bowl.webapp/Controllers/SampleDataController.cs
+++ nfl.fantasy.sanchez.bowl.webapp/nfl.fantasy.sanchez.bowl.webapp/Controllers/SampleDataController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using nfl.fantasy.sanchez.bowl.domain;
 
 namespace nfl.fantasy.sanchez.bowl.webapp.Controllers
 {
@@ -14,31 +16,31 @@ namespace nfl.fantasy.sanchez.bowl.webapp.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        [HttpGet("[action]")]
-        public IEnumerable<WeatherForecast> WeatherForecasts()
+        public Team TeamOne { get; set; }
+        public Team TeamTwo { get; set; }
+
+        private readonly IPlayerDetailsHelper _playerDetailsHelper;
+        private readonly TeamConfig _teamConfigOptions;
+
+        public SampleDataController(IPlayerDetailsHelper playerDetailsHelper, IOptions<TeamConfig> teamConfigAccessor)
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                DateFormatted = DateTime.Now.AddDays(index).ToString("d"),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            });
+            _playerDetailsHelper = playerDetailsHelper;
+            _teamConfigOptions = teamConfigAccessor.Value;
         }
 
-        public class WeatherForecast
+        [HttpGet("[action]")]
+        public async Task<Team> Teams()
         {
-            public string DateFormatted { get; set; }
-            public int TemperatureC { get; set; }
-            public string Summary { get; set; }
-
-            public int TemperatureF
-            {
-                get
-                {
-                    return 32 + (int)(TemperatureC / 0.5556);
-                }
-            }
+            var queryStr = Request.QueryString.Value;
+            var queryParamObj = RequestQueryStrParser<QueryParamObj>.Parse(queryStr);
+            var week = queryParamObj.Week;
+            TeamOne = new Team(TeamIdentifier.Micheal);
+            var rosterOne = await _playerDetailsHelper.GetPlayerDetails(TeamOne.TeamIdentifierIdentifier, week);
+            TeamOne.Roster = rosterOne;
+            return TeamOne;
+            //TeamTwo = new Team(TeamIdentifier.Dustin);
+            //var rosterTwo = await _playerDetailsHelper.GetPlayerDetails(TeamTwo.TeamIdentifierIdentifier, week);
+            //TeamTwo.Roster = rosterTwo;
         }
     }
 }
